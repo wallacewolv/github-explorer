f40a6140194b05ec4b9e4689a287ecb9cc36f071
Wallace <wallace_wolv@hotmail.com>
2021-03-09 16:28:54 -0300

    Utilzando useEffect e chamada de API com fetch

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss'
+
+//  https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: 'unform',
@@ -7,6 +12,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
